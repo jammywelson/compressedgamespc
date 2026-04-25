9e060618fd50ba8df153103ef8fa89782b6dbdd3
jammywelson <letcracks@gmail.com>
2026-04-26 03:06:54 +0500

    fix: Navbar SSR from DB

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -1,15 +1,13 @@
 import NavbarClient from './NavbarClient'
 import { prisma } from '@/lib/prisma'
-
 export const dynamic = 'force-dynamic'
-
+export const revalidate = 0
 async function getSettings() {
   try {
     const s = await prisma.setting.findUnique({ where: { key: 'appearance' } })
     return s ? JSON.parse(s.value) : {}
   } catch { return {} }
 }
-
 export default async function Navbar() {
   const settings = await getSettings()
   return <NavbarClient initialSettings={settings} />
